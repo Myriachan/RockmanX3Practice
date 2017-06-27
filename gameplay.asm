b32d022fd8625a0050bd698b6af2bd3ed4619013
Myria <myriachan@gmail.com>
2017-06-27 01:54:05 -0700

    Disable stage intros and interstage password screen.

--- gameplay.asm
+++ gameplay.asm
@@ -11,3 +11,25 @@
 	and.b #$40
 	rts
 {loadpc}
+
+
+// Disable interstage password screen.
+{savepc}
+	// Always use password screen state 3, which is used to exit to stage select.
+	// States are offsets into a jump table, so they're multiplied by 2.
+	{reorg $00EEF6}
+	ldx.b #3 * 2
+	// Disable fadeout, speeding this up.
+	{reorg $00EFB1}
+	nop
+	nop
+	nop
+{loadpc}
+
+
+// Disable stage intros.
+{savepc}
+	// bne 9597 -> bra 9A53
+	{reorg $009A60}
+	bra $009A53
+{loadpc}
